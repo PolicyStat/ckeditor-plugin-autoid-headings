1ef7c291da6a648994535ca4cc28aebef84214b0
Seth Baughman <sbaughman1@me.com>
2016-09-20 09:55:28 -0400

    Bind value of 'this'.  Add command 'autoid'

--- plugins/autoid/plugin.js
+++ plugins/autoid/plugin.js
@@ -1,16 +1,31 @@
 'use strict';
 
 (function () {
+	var editorHasFocus = false;
+	var commandIsActive = false;
+
 	CKEDITOR.plugins.add('autoid', {
 		init: function (editor) {
+			var self = this;
 
 			if (editor && !editor.config.autoid) {
 				editor.config.autoid = {};
 			}
-			this.settings = editor.config.autoid;
-			if (!this.settings) {
-				this.settings = {};
+			self.settings = editor.config.autoid;
+			if (!self.settings) {
+				self.settings = {};
 			}
+
+			editor.addCommand('autoid', {
+				exec: function (editor) {
+					if (!commandIsActive) {
+						start();
+					} else {
+						stop();
+					}
+				},
+				editorFocus: true
+			});
 
 		}
 	});
