0205105d36611e2aee97dfb16117ddc555060727
Seth Baughman <sbaughman1@me.com>
2016-09-20 14:35:38 -0400

    Add function to generate uuid

--- plugins/autoid/plugin.js
+++ plugins/autoid/plugin.js
@@ -62,6 +62,17 @@
 				heading.setAttributes({ id: uuid });
 			}
 
+			// from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+			function generateUuid() {
+			  function s4() {
+			    return Math.floor((1 + Math.random()) * 0x10000)
+			      .toString(16)
+			      .substring(1);
+			  }
+			  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+			    s4() + '-' + s4() + s4() + s4();
+			}
+
 		}
 	});
 })();
