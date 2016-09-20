d2a984235bb83937e2e5fb0518ae9326d77c8fc0
Seth Baughman <sbaughman1@me.com>
2016-09-20 09:50:06 -0400

    Add config settings

--- plugins/autoid/plugin.js
+++ plugins/autoid/plugin.js
@@ -4,6 +4,14 @@
 	CKEDITOR.plugins.add('autoid', {
 		init: function (editor) {
 
+			if (editor && !editor.config.autoid) {
+				editor.config.autoid = {};
+			}
+			this.settings = editor.config.autoid;
+			if (!this.settings) {
+				this.settings = {};
+			}
+
 		}
 	});
 })();
