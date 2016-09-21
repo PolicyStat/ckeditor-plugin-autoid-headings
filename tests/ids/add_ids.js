6b11536c6844608338e7da292ea57cb1dba71bc2
Seth Baughman <sbaughman1@me.com>
2016-09-21 09:30:21 -0400

    Change tests to wait until the id check is complete to test assertions

--- tests/ids/add_ids.js
+++ tests/ids/add_ids.js
@@ -17,26 +17,38 @@
 
     'test it can add a unique id to a heading': function() {
       var bot = this.editorBot,
+        editor = this.editor,
         heading,
+        resumeAfter = bender.tools.resumeAfter,
         startHtml = '<h1>This is a heading</h1>';
 
       bot.setHtmlWithSelection(startHtml);
 
-      heading = this.editor.editable().findOne('h1');
+      resumeAfter(editor, 'allIdsComplete', function() {
+        heading = editor.editable().findOne('h1');
 
-      assert.isTrue(heading.hasAttribute('id'));
+        assert.isTrue(heading.hasAttribute('id'));
+      })
+
+      wait();
     },
 
     'test it does not change the id of a heading that already has one': function() {
       var bot = this.editorBot,
+        editor = this.editor,
         heading,
+        resumeAfter = bender.tools.resumeAfter,
         startHtml = '<h1 id="12345">Heading with id</h1>';
 
       bot.setHtmlWithSelection(startHtml);
 
-      heading = this.editor.editable().findOne('h1');
+      resumeAfter(editor, 'allIdsComplete', function() {
+        heading = editor.editable().findOne('h1');
 
-      assert.areSame(heading.getId(), '12345');
+        assert.areSame(heading.getId(), '12345');
+      })
+
+      wait();
     }
   });
 })();
