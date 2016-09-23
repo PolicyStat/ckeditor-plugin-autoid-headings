7f0c7caef9d1c7e13afe704acc3be08e2ec72e07
Seth Baughman <sbaughman1@me.com>
2016-09-23 12:00:08 -0400

    Fix full copy test so it actually tests what it's supposed to
    Also, WIP test for pasting heading with different ID

--- tests/paste_events/paste_events.js
+++ tests/paste_events/paste_events.js
@@ -21,40 +21,69 @@
       this.editor.execCommand('autoid');
     },
 
-    'test copied header will have a new id when a full copy is made': function() {
+    'test pasted heading id does not change when it is not a duplicate id': function() {
       var bot = this.editorBot,
         editor = bot.editor,
         heading,
         headings,
         resumeAfter = bender.tools.resumeAfter,
-        headingWithId = '<h1 id="12345">This is a heading^</h1>';
+        headingWithId = '<h1 id="12345">This is a heading^</h1>',
+        headingWithDifferentId = '<h1 id="678910">This is also a heading</h1>';
 
       bot.setHtmlWithSelection(headingWithId);
+
+      editor.execCommand('autoid');
+
+      editor.execCommand('paste', headingWithDifferentId);
+
+      headings = editor.editable().find('h1');
+      heading = headings.getItem(1);
+
+      assert.areSame('678910', heading.getAttribute('id'));
+    },
+
+    'test pasted heading gets new id when it is a full copy of an existing heading': function() {
+      var bot = this.editorBot,
+        editor = bot.editor,
+        heading,
+        headings,
+        resumeAfter = bender.tools.resumeAfter,
+        headingWithId = '<h1 id="12345">This is a heading^</h1>',
+        identicalHeading = '<h1 id="12345">This is a heading</h1>';
+
+      bot.setHtmlWithSelection(headingWithId);
+
+      // hit enter key to prevent pasting text in same heading element
+      editor.editable().fire('keydown', new CKEDITOR.dom.event({
+						keyCode: 13,
+						ctrlKey: false,
+						shiftKey: false
+					}));
 
       resumeAfter(editor, 'allIdsComplete', function() {
         heading = editor.editable().findOne('h1');
 
         // verify original heading still has same id
         assert.areSame('12345', heading.getAttribute('id'));
+
+        resumeAfter(editor, 'idAdded', function() {
+          headings = editor.editable().find('h1');
+          // get pasted heading (second heading on page)
+          heading = headings.getItem(1);
+
+          // verify pasted heading does not have the original id
+          assert.areNotSame('12345', heading.getAttribute('id'));
+        });
+
+        editor.execCommand('paste', identicalHeading);
+
+        // paste identical heading, wait for new id to be added to the new copy
+        wait();
       });
 
       editor.execCommand('autoid');
 
       // wait for initial id assignment for all headings to complete
-      wait();
-
-      resumeAfter(editor, 'idAdded', function() {
-        headings = editor.editable().find('h1');
-        // get pasted heading (second heading on page)
-        heading = headings.getItem(1);
-
-        // verify pasted heading does not have the original id
-        assert.areNotSame('12345', heading.getAttribute('id'));
-      });
-
-      editor.execCommand('paste', headingWithId);
-
-      // paste identical heading, wait for new id to be added to the new copy
       wait();
     }
 
