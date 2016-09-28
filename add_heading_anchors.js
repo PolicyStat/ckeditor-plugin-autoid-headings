9cd21334d420b378e8dd66aaaf2698ed96a19a4f
Seth Baughman <sbaughman1@me.com>
2016-09-28 11:37:39 -0400

    Get headings and createAnchor for each one that has an id

--- add_heading_anchors.js
+++ add_heading_anchors.js
@@ -5,6 +5,20 @@ var addHeadingAnchors = {
     if (this.target) {
       this.addAnchorsToHeadings();
     }
+  },
+
+  addAnchorsToHeadings: function () {
+    var headings = this.target.querySelectorAll('h1, h2, h3, h4, h5, h6'),
+      id;
+
+    headings.forEach(function (heading) {
+      if (id = heading.id) {
+        var anchor = this.createAnchor(id);
+      }
+    }.bind(this));
+  },
+
+  createAnchor: function (id) {
   }
 
 };
