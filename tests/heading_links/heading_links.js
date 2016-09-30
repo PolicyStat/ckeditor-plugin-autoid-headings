99482b82c38cc76980dd08d482223c1ff30371aa
Seth Baughman <sbaughman1@me.com>
2016-09-30 11:36:23 -0400

    Fix inline test expectation

--- tests/heading_links/heading_links.js
+++ tests/heading_links/heading_links.js
@@ -76,8 +76,8 @@
 
 		'test discovery of headings (inline)': function() {
 			assertHeadingDiscovery( this.editorBots.inline,
-				[ 'ma', 'na', 'oa', '' ],
-				[ 'm', 'n', '', '' ] );
+				[ 'ma', 'na', 'oa', 'ta', 'ua', 'wa', '' ],
+				[ 'm', 'n', '', 't', 'u', '', '' ] );
 		},
 
 		'test discovery of headings (divarea)': function() {
