b7a8dc7aad5da99a9e922e3dc1cc184249688479
Alex Tsai <caffodian@gmail.com>
2016-11-10 13:41:52 -0800

    start hooking up tests

---
+++ viewer/test/test_add_heading_anchors.js
@@ -0,0 +1,9 @@
+describe('addHeadingAnchors', function() {
+  it('should do stuff', function() {
+      console.log('asdf');
+  });
+
+  it('should add headings', function() {
+      addHeadingAnchors.init('blah');
+  })
+});
