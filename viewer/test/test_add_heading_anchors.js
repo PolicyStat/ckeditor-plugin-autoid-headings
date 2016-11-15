e8254db0bd9128ca0db04f5540073ed14c7c2e3a
Alex Tsai <caffodian@gmail.com>
2016-11-15 08:42:49 -0800

    test actually does things now.

--- viewer/test/test_add_heading_anchors.js
+++ viewer/test/test_add_heading_anchors.js
@@ -1,9 +1,28 @@
-describe('addHeadingAnchors', function() {
-  it('should do stuff', function() {
-      console.log('asdf');
-  });
+var assert = chai.assert;
 
-  it('should add headings', function() {
-      addHeadingAnchors.init('#testarea');
-  })
+describe('addHeadingAnchors', function() {
+    addHeadingAnchors.init('#testarea');
+
+    it('added the header links beside each heading', function() {
+        var headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+
+        headings.forEach(function assertHasCopyLink(heading) {
+            var anchor = heading.lastChild;
+            var href;
+
+            // is element
+            assert.equal(anchor.nodeType, 1);
+
+            // is an anchor
+            assert.equal(anchor.nodeName, 'A');
+
+            // href is to id
+
+            href = anchor.getAttribute('href');
+
+            assert.endsWith(href, "#" + heading.getAttribute('id'));
+
+        })
+
+    });
 });
