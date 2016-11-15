b3c81ca36902904bda6b3ff96870f2cc7fa4407c
Alex Tsai <caffodian@gmail.com>
2016-11-15 10:10:50 -0800

    test re-org

--- viewer/test/test_add_heading_anchors.js
+++ viewer/test/test_add_heading_anchors.js
@@ -1,71 +1,84 @@
 var assert = chai.assert;
 
-describe('addHeadingAnchors', function() {
-    before(function() {
+describe('addHeadingAnchors', function () {
+    before(function () {
         addHeadingAnchors.init('#testarea');
-        this.headingsWithAnId = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
-        this.clipboardAnchors = document.querySelectorAll('a[data-clipboard-text]');
     });
 
-    it('added the header links beside each heading', function() {
-        this.headingsWithAnId.forEach(function assertHasCopyLink(heading) {
-            var anchor = heading.lastChild,
-                href,
-                clipboardDataAttribute;
+    describe('HTML modification', function () {
+        before(function() {
+            this.headingsWithAnId = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+        });
 
-            // is element
-            assert.equal(anchor.nodeType, 1);
+        it('added the header links beside each heading', function () {
+            this.headingsWithAnId.forEach(function assertHasCopyLink(heading) {
+                var anchor = heading.lastChild,
+                    href,
+                    clipboardDataAttribute;
 
-            // is an anchor
-            assert.equal(anchor.nodeName, 'A');
+                // is element
+                assert.equal(anchor.nodeType, 1);
 
-            // href is to id
+                // is an anchor
+                assert.equal(anchor.nodeName, 'A');
 
-            href = anchor.getAttribute('href');
+                // href is to id
 
-            assert.endsWith(href, "#" + heading.getAttribute('id'));
+                href = anchor.getAttribute('href');
 
-            // data-clipboard-text is the full url
+                assert.endsWith(href, "#" + heading.getAttribute('id'));
 
-            clipboardDataAttribute = anchor.getAttribute('data-clipboard-text');
+                // data-clipboard-text is the full url
 
-            // this is a crappy/quick way to assert for the full url being in the data attribute.
-            assert.startsWith(clipboardDataAttribute, 'http://');
-            assert.endsWith(clipboardDataAttribute, "#" + heading.getAttribute('id'));
+                clipboardDataAttribute = anchor.getAttribute('data-clipboard-text');
+
+                // this is a crappy/quick way to assert for the full url being in the data attribute.
+                assert.startsWith(clipboardDataAttribute, 'http://');
+                assert.endsWith(clipboardDataAttribute, "#" + heading.getAttribute('id'));
+            });
         });
     });
 
-    it('changes the address bar when clicking on links', function() {
-        this.clipboardAnchors.forEach(function assertClipboardCopy(anchor) {
-            var href = anchor.getAttribute('href');
-            console.log(href);
-            anchor.click();
+    describe('click handling', function () {
+        before(function() {
+            this.clipboardAnchors = document.querySelectorAll('a[data-clipboard-text]');
+        })
 
-            // assert the address bar changed
+        it('changes the address bar when clicking on links', function () {
+            this.clipboardAnchors.forEach(function assertClipboardCopy(anchor) {
+                var href = anchor.getAttribute('href');
+                console.log(href);
+                anchor.click();
 
-            assert(window.location.hash, href);
+                // assert the address bar changed
+
+                assert(window.location.hash, href);
+            });
         });
-    });
 
-    it('copies to clipboard when clicking on links', function() {
-        // we can't directly assert on the real clipboard thanks to https://w3c.github.io/editing/execCommand.html#dfn-the-copy-command
+        it('copies to clipboard when clicking on links', function () {
+            // we can't directly assert on the real clipboard thanks to https://w3c.github.io/editing/execCommand.html#dfn-the-copy-command
 
-        this.clipboardAnchors.forEach(function(anchor) {
+            this.clipboardAnchors.forEach(function (anchor) {
 
-            // register a one time event handler to check the event text
-            // this isn't a very good test due to the limitations.
-            // it basically checks that yes, clipboardjs was init and
-            // catches clicks on all the anchors.
-            addHeadingAnchors.handler.once('error', function(e) {
-                var url = e.text;
+                // Register a one time event handler to check the event text
+                // this isn't a very good test due to the limitations.
+                // It basically checks that yes, clipboardjs was init and
+                // catches clicks on all the anchors.
+                addHeadingAnchors.handler.once('error', function (e) {
+                    var url = e.text;
 
-                assert.equal(url, anchor.getAttribute('data-clipboard-text'));
+                    assert.equal(url, anchor.getAttribute('data-clipboard-text'));
 
-                e.clearSelection();
+                    e.clearSelection();
+                });
+
+                anchor.click();
             });
 
-            anchor.click();
         });
-
-    });
+    })
 });
+
+
+
