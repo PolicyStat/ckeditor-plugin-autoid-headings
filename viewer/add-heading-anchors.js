1299664338f414f99dd9f78f15c6107076f022f6
Alex Tsai <caffodian@gmail.com>
2016-11-15 13:42:35 -0800

    more linting (yea I forgot to commit the rename...)

---
+++ viewer/add-heading-anchors.js
@@ -0,0 +1,49 @@
+var addHeadingAnchors = {
+
+  init: function (selector) {
+    this.target = document.querySelector(selector);
+    if (this.target) {
+      this.addAnchorsToHeadings();
+      this.registerClipboardHandler();
+    }
+  },
+
+  addAnchorsToHeadings: function () {
+    var selectorString = "h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]";
+    var headings = this.target.querySelectorAll(selectorString);
+
+    headings.forEach(function (heading) {
+      var anchor = this.createAnchor(heading.id);
+      heading.appendChild(anchor);
+    }.bind(this));
+  },
+
+  createAnchor: function (id) {
+    var anchor = document.createElement("a");
+    var icon = document.createElement("i");
+    var attributes = {
+      href: "#" + id,
+      class: "headerLink",
+      title: "Permalink to this headline"
+    };
+
+    icon.setAttribute("class", "icon-share");
+
+    anchor.appendChild(icon);
+
+    for (var attr in attributes) {
+      anchor.setAttribute(attr, attributes[attr]);
+    }
+
+    // there might be a browser quirk here
+    anchor.setAttribute("data-clipboard-text", anchor.href);
+
+    return anchor;
+  },
+
+  registerClipboardHandler: function () {
+    if (!this.handler) {
+      this.handler = new Clipboard("a.headerLink");
+    }
+  }
+};
---
+++ viewer/test/test-add-heading-anchors.js
@@ -0,0 +1,119 @@
+/* global addHeadingAnchors:false */
+
+var assert = chai.assert;
+
+describe("addHeadingAnchors", function () {
+  before(function () {
+    this.testArea = document.getElementById("testarea");
+    addHeadingAnchors.init("#testarea");
+  });
+
+  describe("HTML modification", function () {
+    before(function () {
+      this.headingsWithAnId = this.testArea.querySelectorAll(
+        "h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]"
+      );
+      this.headingsWithoutAnId = this.testArea.querySelectorAll(
+        "h1:not([id]), h2:not([id]), h3:not([id]), h4:not([id]), h5:not([id]), h6:not([id])"
+      );
+    });
+
+    it("added the header links beside each heading", function () {
+      var assertHasCopyLink = function (heading) {
+        var anchor = heading.lastChild;
+        var href;
+        var clipboardDataAttribute;
+
+        // is element
+        assert.equal(anchor.nodeType, 1);
+
+        // is an anchor
+        assert.equal(anchor.nodeName, "A");
+
+        // href is to id
+
+        href = anchor.getAttribute("href");
+
+        assert.endsWith(href, "#" + heading.getAttribute("id"));
+
+        // data-clipboard-text is the full url
+
+        clipboardDataAttribute = anchor.getAttribute("data-clipboard-text");
+
+        // this is a crappy/quick way to assert for the full url being in the data attribute.
+        assert.startsWith(clipboardDataAttribute, "http://");
+        assert.endsWith(clipboardDataAttribute, "#" + heading.getAttribute("id"));
+      };
+
+      this.headingsWithAnId.forEach(assertHasCopyLink);
+    });
+
+    it("did not add anchors inside non-id headings", function () {
+      var assertNoAnchorAdded = function (heading) {
+        // we can just assert there is no anchor, because the starting html doesn't have it.
+        var anchor = heading.querySelector("a");
+
+        assert.isNull(anchor);
+      };
+
+      this.headingsWithoutAnId.forEach(assertNoAnchorAdded);
+    });
+  });
+
+  describe("click handling", function () {
+    before(function () {
+      this.clipboardAnchors = this.testArea.querySelectorAll("a[data-clipboard-text]");
+    });
+
+    it("changes the address bar when clicking on links", function () {
+      this.clipboardAnchors.forEach(function assertClipboardCopy(anchor) {
+        var href = anchor.getAttribute("href");
+        anchor.click();
+
+        // assert the address bar changed
+
+        assert(window.location.hash, href);
+      });
+    });
+
+    it("copies to clipboard when clicking on links", function (done) {
+      // a quick alternative to spying
+      var callCount = 0;
+
+      this.clipboardAnchors.forEach(function (anchor) {
+
+        // Register a one time event handler to check the event text
+        // this isn't a very good test due to the limitations.
+        // It basically checks that yes, clipboardjs was init and
+        // catches clicks on all the anchors.
+
+        // We have to use the error event,
+        // because it will never succeed without an actual click.
+        // see https://w3c.github.io/editing/execCommand.html#dfn-the-copy-command
+
+        addHeadingAnchors.handler.once("error", function (e) {
+          var url = e.text;
+
+          assert.equal(url, anchor.getAttribute("data-clipboard-text"));
+
+          e.clearSelection();
+
+          callCount += 1;
+
+          // If we somehow don't add all 6 headings,
+          // this test will fail because the callback is not fired
+          if (callCount === 6) {
+            done();
+          }
+        });
+
+        // click on all the anchors
+
+        anchor.click();
+      });
+
+    });
+  });
+});
+
+
