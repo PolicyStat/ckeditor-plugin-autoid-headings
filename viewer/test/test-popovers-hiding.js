9a165ddd9aa0bc592a8522e726e14f8bb37f60e1
Alex Tsai <caffodian@gmail.com>
2016-11-23 10:30:02 -0800

    renameed the file, we no longer need sinon to test this.

--- viewer/test/test-popovers-hiding.js
+++ viewer/test/test-popovers-hiding.js
@@ -1,28 +1,18 @@
-/* global addHeadingAnchors:false sinon:false */
+/* global addHeadingAnchors:false */
 
 var assert = chai.assert;
 
 describe("popover timing", function () {
-  var INTERACTION_TIMEOUT = 1000;
-  var clock;
-
   before(function () {
     this.testArea = document.getElementById("test-popovers-timing");
     addHeadingAnchors.init("#test-popovers-timing", "#test-popovers-timing .popovers");
-    this.clipboardAnchors = this.testArea.querySelectorAll("a[data-clipboard-text]");
-  });
-
-  beforeEach(function () {
-    clock = sinon.useFakeTimers();
-  });
-
-  afterEach(function () {
-  //  clock.restore();
+    this.firstClipboardAnchor = this.testArea.querySelector("a[data-clipboard-text]");
+    this.unrelatedHeading = this.testArea.querySelector("h2");
   });
 
   it("displays the popover, which then fades when the input is blurred", function () {
-    var firstAnchor = this.clipboardAnchors[0];
-    var input;
+    var firstAnchor = this.firstClipboardAnchor;
+    var unrelatedHeading = this.unrelatedHeading;
     var popover;
 
     firstAnchor.click();
@@ -32,21 +22,12 @@ describe("popover timing", function () {
 
     popover = this.testArea.querySelector(".popovers .popover");
 
-    // click into the text area
-    input = popover.querySelector("input");
-    input.focus();
-
-    // wait more than the timeout
-    clock.tick(INTERACTION_TIMEOUT);
-
     // check the popover is still visible
     assert.isTrue(popover.classList.contains("in"), "popover is not hidden");
 
-    // focus out of the popover
-    input.blur();
+    // click something else
 
-    // wait for timeout
-    clock.tick(INTERACTION_TIMEOUT);
+    unrelatedHeading.click();
 
     // check popover is hidden
     assert.isFalse(popover.classList.contains("in"), "popover is not hidden");
