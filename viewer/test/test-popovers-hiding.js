e8a87bbb3f1cd2b2e945151662c8b8160cddef61
Alex Tsai <caffodian@gmail.com>
2016-11-23 10:33:30 -0800

    add another test

--- viewer/test/test-popovers-hiding.js
+++ viewer/test/test-popovers-hiding.js
@@ -10,7 +10,7 @@ describe("popover timing", function () {
     this.unrelatedHeading = this.testArea.querySelector("h2");
   });
 
-  it("displays the popover, which then fades when the input is blurred", function () {
+  it("displays the popover, which then fades when something else is clicked", function () {
     var firstAnchor = this.firstClipboardAnchor;
     var unrelatedHeading = this.unrelatedHeading;
     var popover;
@@ -30,7 +30,34 @@ describe("popover timing", function () {
     unrelatedHeading.click();
 
     // check popover is hidden
-    assert.isFalse(popover.classList.contains("in"), "popover is not hidden");
+    assert.isFalse(popover.classList.contains("in"), "popover is now hidden");
+
+  });
+
+  it("displays the popover and does not blur if the popover itself is clicked", function () {
+    var firstAnchor = this.firstClipboardAnchor;
+    var popover;
+
+    firstAnchor.click();
+
+    // make sure nothing crazy happened like failed test cleanup
+    assert.equal(1, this.testArea.querySelector(".popovers").children.length);
+
+    popover = this.testArea.querySelector(".popovers .popover");
+
+    // check the popover is still visible
+    assert.isTrue(popover.classList.contains("in"), "popover is not hidden");
+
+    // click on the popover
+
+    popover.click();
+
+    // click on the anchor again
+
+    firstAnchor.click();
+
+    // check popover is hidden
+    assert.isTrue(popover.classList.contains("in"), "popover is not hidden");
 
   });
 
