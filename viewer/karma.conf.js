f75567caa9c211a18cfc3357c8147e935ec1ef71
Alex Tsai <caffodian@gmail.com>
2017-04-03 13:51:59 -0700

    add to karma frameworks

--- viewer/karma.conf.js
+++ viewer/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["mocha", "chai", "fixture"],
+    frameworks: ["mocha", "chai-spies", "chai", "fixture"],
 
 
     // list of files / patterns to load in the browser
