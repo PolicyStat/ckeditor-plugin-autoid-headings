51202d9a8364ec8bc4c4a31e5ce39645be80eac3
Alex Tsai <caffodian@gmail.com>
2017-04-05 15:42:11 -0700

    wrong getter

--- tests/ids/add_ids.js
+++ tests/ids/add_ids.js
@@ -157,7 +157,7 @@
         headings = editor.editable().find('h1');
 
         for (var i = 0; i < headings.length; i++) {
-          heading = headings[i];
+          heading = headings.getItem(i);
           assert.isTrue(heading.hasAttribute('id'));
           currentId = heading.getAttribute('id');
           assert.areSame(-1, ids.indexOf(currentId));
